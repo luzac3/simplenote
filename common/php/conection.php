d87001f16281e67e94f426890f71c5e4a09242c9
user <user@192.168.10.106>
2018-05-20 19:29:11 +0900

    ローカルとサーバでの接続先を自動変更するように設定

--- common/php/conection.php
+++ common/php/conection.php
@@ -2,12 +2,15 @@
 
 function db_connect($db_name){
     // db接続関数
-    $server ="mysql622.db.sakura.ne.jp";
-    // $server ="localhost";
-    // $username="root";
-    $username="wolfnet-twei";
-    // $password="alderaan";
-    $password="alderaan123";
+    if ($_SERVER['REQUEST_URI'] == "localhost"){
+        $server ="localhost";
+        $username="root";
+        $password="alderaan";
+    }else{
+        $server ="mysql622.db.sakura.ne.jp";
+        $username="wolfnet-twei";
+        $password="alderaan123";
+    }
 
     $mysqli = new mysqli($server,$username,$password,$db_name);
 
